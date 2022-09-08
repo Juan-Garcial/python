d549242161ad66ae53aa48bf9b4ccfc374f4c746
juan garcia <jcarlos,garcia@alumnos.udg.mx>
2022-09-08 13:41:02 -0500

    add waveform generator and set frecuancy to 200hz

--- main.py
+++ main.py
@@ -3,9 +3,15 @@ import time
 import pyvisa
 
 rm = pyvisa.ResourceManager()
+"""
 source_2 = rm.open_resource('GPIB0::20::INSTR')
 #print(inst.query("*IDN?"))
 
 source_1 = rm.open_resource('GPIB0::19::INSTR')
 source_1.write('VOLT:LEV 9')
 source_2.write('VOLT:LEV 6')
+"""
+
+W_gen_1 = rm.open_resource('TCPIP0::10.0.7.161::inst0::INSTR')
+print(W_gen_1.query("*IDN?"))
+W_gen_1.write('SOURce1:FREQ 200 ')
